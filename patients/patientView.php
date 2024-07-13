bd72a42eb8a083377463dcd88a3c45349e41d31f
Con5lata <131604666+Con5lata@users.noreply.github.com>
2024-07-13 22:38:19 +0300

    final changes... i Think

--- patients/patientView.php
+++ patients/patientView.php
@@ -155,7 +155,7 @@ $ID = $_SESSION['userid'];
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="inquiryModalLabel">Medical Inquiry</h5>
+                <h5 class="modal-title" id="inquiryModalLabel">Do you have a Medical Question?</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
